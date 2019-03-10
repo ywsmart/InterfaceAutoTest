01f3806b809e0e9c523cf3bb7b4647d8490070a6
ywsmart <ywsmart@163.com>
2019-03-10 17:34:59 +0800

    初始提交

---
+++ src/main/java/GetResource/GetTop.java
@@ -0,0 +1,67 @@
+package GetResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import Common.Commons;
+import performance.Main;
+
+public class GetTop {
+	private static  double Cpu = 0;
+
+	public static void main( String[] args ) throws IOException{
+		for ( int i = 0; i < 10; i++ ){
+			System.out.println( " Cpu：" + topCpu( "com.ss.android.ugc.aweme" ) );
+		}
+	}
+
+	  public static double topCpu(String packageName) throws IOException {
+		  String cpuStr="0";
+		  try{ 
+		    Runtime runtime = Runtime.getRuntime();
+		    Process proc = runtime.exec("adb -s "+Main.devices+" shell top -n 1| grep "+packageName);
+//		    Process proc = runtime.exec("adb -s "+Main.devices+" shell top -n 1| findstr "+packageName);
+		    try {
+		        if (proc.waitFor() != 0) {
+		            System.err.println("exit value = " + proc.exitValue());
+		        }
+		        BufferedReader in = new BufferedReader(new InputStreamReader( proc.getInputStream()));
+		        String line = null;
+		        while ((line = in.readLine()) != null) {
+//					System.err.println("line:"+line);
+					if(line.contains("%")&&line.contains(packageName)&&line.contains("/")==false){
+						List<String> strList = Arrays.asList(line.split("\\s+"));
+						if(strList.get(2).trim().contains("%")){
+							cpuStr = strList.get(2).trim().replace("%", "");
+						}
+						if(strList.get(1).trim().contains("%")) {
+							cpuStr = strList.get(1).trim().replace("%", "");
+						}
+						break;
+		        	}
+		        }
+		    	Cpu=Double.parseDouble( cpuStr );
+		    } catch (InterruptedException e) {
+		        System.err.println(e);
+		    }finally{
+		        try {
+		            proc.destroy();
+		        } catch (Exception e2) {
+		        }
+		    }}
+			catch ( Exception StringIndexOutOfBoundsException ){
+				System.out.print( "请检查设备是否连接TopCpu" );
+				return -0.1;
+			}
+			return Commons.streamDouble(Cpu);
+	  }
+
+
+
+
+}
+
+
